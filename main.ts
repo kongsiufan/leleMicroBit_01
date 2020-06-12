6b72eae036f7d01654b16ea323336561eb5bea20
kongsiufan <66825829+kongsiufan@users.noreply.github.com>
2020-06-12 20:22:50 +0800

    更新 main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -4,6 +4,7 @@ input.onButtonPressed(Button.A, function () {
 function playMusic01 () {
     music.playTone(262, music.beat(BeatFraction.Whole))
     music.playTone(523, music.beat(BeatFraction.Whole))
+    music.playMelody("B E G A G B C5 G ", 120)
 }
 basic.forever(function () {
 	
